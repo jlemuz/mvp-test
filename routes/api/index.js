1720b476d653ddd0080f6e3382cf0fd41d3324cf
Jose Lemuz <lemuz.jose@gmail.com>
2022-04-01 04:53:56 -0700

    added post crud

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 
 //takes the routes defined in ./user-routes.js
 const userRoutes = require('./user-routes.js');
+
+const postRoutes = require('./post-routes');
 //prefixes the endpoint with /users/
 //users/1
 router.use('/users', userRoutes);
+
+router.use('/posts', postRoutes);
+
 
 module.exports = router;
---
+++ routes/api/post-routes.js
@@ -0,0 +1,145 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+const path = require("path");
+const util = require("util");
+
+
+// get all users
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: ['id', 'post_url', 'title', 'img_url', 'created_at'],
+      order: [['created_at', 'DESC']], 
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+  router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_url', 'title', 'img_url', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.post('/', (req, res) => {
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.body.user_id,
+      img_url: req.body.img_url
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.post("/img", async (req,res)=>{
+
+    try{
+        const file = req.files.file;
+        const fileName = file.name;
+        const size = file.data.length;
+        const extension = path.extname(fileName);
+
+        const allowedExtensions = /png|jpeg|jpg|gif/;
+
+        if (!allowedExtensions.test(extension)) throw "Unsupported extension!";
+        if (size > 5000000) throw "File must be less than 5MB";
+
+
+        const md5 = file.md5;
+        const URL = "/uploads/" + md5 + extension;
+
+        await util.promisify(file.mv)("./public" + URL);
+        res.json({
+            message: "File uploaded successfully",
+            url: URL,
+        });
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: err,
+        })
+    }
+
+})
+
+router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+  module.exports = router;
